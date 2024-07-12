b7ed5e296adb55c51447d94aa839465215d6b96d
flo <f.imbert4@gmail.com>
2024-07-12 17:32:29 +0200

    deploying test

--- constants/index.js
+++ constants/index.js
@@ -1,17 +1,16 @@
-// require("dotenv").config();
 
-// const {
-//   NEXT_PUBLIC_MARKETPLACE_ADDRESS,
-//   NEXT_PUBLIC_NFT_ADDRESS
-// } = process.env;
+const {
+  NEXT_PUBLIC_MARKETPLACE_ADDRESS,
+  NEXT_PUBLIC_NFT_ADDRESS
+} = process.env;
 
 // export const contractMarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // hardhat
-export const contractMarketplaceAddress = "0x26f11f946f25354E67795eed4fE06972179258B2"; // baseSepolia
+// export const contractMarketplaceAddress = "0x26f11f946f25354E67795eed4fE06972179258B2"; // baseSepolia
 // export const contractNFTAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // hardhat
-export const contractNFTAddress = "0x84F6b5d6f0bFd30c26D33BE7573Aab6663EF561B"; // baseSepolia
+// export const contractNFTAddress = "0x84F6b5d6f0bFd30c26D33BE7573Aab6663EF561B"; // baseSepolia
 
-// export const contractMarketplaceAddress = process.env.NODE_ENV === "production" ? NEXT_PUBLIC_MARKETPLACE_ADDRESS : "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-// export const contractNFTAddress = process.env.NODE_ENV === "production" ? NEXT_PUBLIC_NFT_ADDRESS : "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const contractMarketplaceAddress = process.env.NODE_ENV === "production" ? NEXT_PUBLIC_MARKETPLACE_ADDRESS : "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const contractNFTAddress = process.env.NODE_ENV === "production" ? NEXT_PUBLIC_NFT_ADDRESS : "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 
 export const contractMarketplaceAbi = [
